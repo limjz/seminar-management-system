46ce31fe98f3ed032401b97fc9e8f359cebe1920
limjz <limjunzhao1@gmail.com>
2026-01-10 23:26:20 +0800

    add the coordinator dashboard
    now can from the coordinator dashboard then click button to direct to the create session page

--- src/main.java
+++ src/main.java
@@ -1,10 +1,10 @@
 
-import views.CoordinatorDashboard;
+import views.*;
 
 public class main {
   public static void main(String[] args) {
       
-    
+
     CoordinatorDashboard coordinatorDashboard = new CoordinatorDashboard();
     coordinatorDashboard.setVisible(true);
     
